c5e8488cd31cdba98a4233d35e4cb47aa0d2c261
getman-cnii <getmanartem@gmail.com>
2014-07-31 17:48:15 +0400

    Initial commit

---
+++ src/getman/calc/Master.java
@@ -0,0 +1,16 @@
+package getman.calc;
+
+/**@author ParfyonovArtem
+ * @version 1.0
+ * @since 30-07-2014*/
+
+/**Master class of the application*/
+public class Master {
+
+	/** Applicatiob entry point
+	 * @param args*/
+	public static void main(String[] args) {
+		
+	}
+
+}
